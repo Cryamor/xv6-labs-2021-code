3871c2de56205a02a61054872b6d7c7ddee49164
zmc <1507007359@qq.com>
2023-08-17 16:41:12 +0800

    thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -36,11 +36,13 @@ insert(int key, int value, struct entry **p, struct entry *n)
   *p = e;
 }
 
+pthread_mutex_t lock[NBUCKET];
+
 static 
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+  pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,20 +56,19 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
-
+  pthread_mutex_lock(&lock[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+  pthread_mutex_unlock(&lock[i]);
   return e;
 }
 
--- user/uthread.c
+++ user/uthread.c
@@ -10,10 +10,28 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  /*   0 */ uint64 ra;
+  /*   8 */ uint64 sp;
+  // callee
+  /*  16 */ uint64 s0;
+  /*  24 */ uint64 s1;
+  /*  32 */ uint64 s2;
+  /*  40 */ uint64 s3;
+  /*  48 */ uint64 s4;
+  /*  56 */ uint64 s5;
+  /*  64 */ uint64 s6;
+  /*  72 */ uint64 s7;
+  /*  80 */ uint64 s8;
+  /*  88 */ uint64 s9;
+  /*  96 */ uint64 s10;
+  /* 104 */ uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +80,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&t->ctx, (uint64)&current_thread->ctx);
   } else
     next_thread = 0;
 }
@@ -76,6 +95,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;
+  t->ctx.sp = (uint64)&t->stack + STACK_SIZE - 1;
 }
 
 void 
